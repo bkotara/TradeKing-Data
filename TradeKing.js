19deb952d3aa7b82a105ec1147955de4a41deb27
Brandon Kotara <brandon@kotara.com>
2015-08-17 15:27:37 -0500

    API now uses promises

--- TradeKing.js
+++ TradeKing.js
@@ -8,64 +8,8 @@ var _ = require('underscore');
 var TradeKing = function(credentials, symbols, fields) {
   this.connection = new OAuth(null, null, credentials.consumer_key, credentials.consumer_secret, "1.0", null, "HMAC-SHA1");
   this._credentials = credentials
-  this.streamIsRunning = false;
-  this.stream = null;
   this.symbols = (_.isArray(symbols)) ? symbols.join():null;
   this.fields = (_.isArray(fields)) ? fields.join():null;
-}
-
-/**
- * Parameters: array symbols, function callback(error, response).
- * If symbols is not provided, pass null for its value.
- * The callback function is required.
- **/
-TradeKing.prototype.openStream = function(symbols, callback) {
-  symbols = (_.isArray(symbols)) ? symbols.join():this.symbols;
-
-  if(!_.isString(symbols) || !_.isFunction(callback)) {
-    //error, symbols and callback are needed for this method
-    throw new Error("Symbols and a callback funtion are reuired for opening streams.")
-  }
-
-  var url = "https://stream.tradeking.com/v1/market/quotes.json?symbols=" + symbols;
-  var request = this.connection.get(url, 
-    this._credentials.access_token, 
-    this._credentials.access_secret);
-
-  request.on('response', function(response) {
-    response.setEncoding('utf8');
-    response.on('data', function(data) {
-      var result;
-      try {
-        result = JSON.parse(data);
-        process.nextTick(function() {
-          callback(null, result)
-        });
-      }
-      catch(error) {
-        process.nextTick(function() {
-          callback(error)
-        });
-      }
-    });
-    this.stream = response
-    this.streamIsRunning = true
-  });
-  request.end();
-}
-
-TradeKing.prototype.pauseStream = function() {
-  if(this.streamIsRunning === true) {
-    this.stream.pause();
-    this.streamIsRunning = false;
-  }
-}
-
-TradeKing.prototype.resumeStream = function() {
-  if(this.streamIsRunning === false) {
-    this.stream.resume();
-    this.streamIsRunning = true;
-  }
 }
 
 TradeKing.prototype.addSymbols = function() {
@@ -115,48 +59,96 @@ TradeKing.prototype.setFields = function(fields) {
 }
 
 /**
- * Parameters: array symbols, array fields, function callback(error, response).
- * If symbols or fields are not provided, pass null for their values.
+ * Parameters: array symbols
+ * Returns a promise with resolve(stream) and reject(error) as the possible outcomes
  **/
-TradeKing.prototype.getStockData = function(symbols, fields, callback) {
+TradeKing.prototype.openStream = function(symbols) {
   symbols = (_.isArray(symbols)) ? symbols.join():this.symbols;
 
-  if(!_.isString(symbols) || !_.isFunction(callback)) {
+  if(!_.isString(symbols)) {
     //error, symbols and callback are needed for this method
-    throw new Error("Symbols and a callback funtion are reuired for opening streams.")
+    throw new Error("Symbols are reuired for opening streams.")
+  }
+
+  var url = "https://stream.tradeking.com/v1/market/quotes.json?symbols=" + symbols;
+  return new Promise(function(resolve, reject) {
+    var request = this.connection.get(url, this._credentials.access_token, this._credentials.access_secret);
+    request.on('response', function(response) {
+      response.setEncoding('utf8');
+      resolve(response)
+    });
+
+    request.on("error", function(error) {
+      reject(error)
+    });
+
+    request.end();
+  }.bind(this));
+}
+
+/**
+ * Parameters: array symbols, array fields
+ * Returns a promise with resolve(response as JSON) and reject(error) as the possible outcomes
+ **/
+TradeKing.prototype.getStockData = function(symbols, fields) {
+  symbols = (_.isArray(symbols)) ? symbols.join():this.symbols;
+
+  if(!_.isString(symbols)) {
+    //error, symbols and callback are needed for this method
+    throw new Error("Symbols are reuired for opening streams.")
   }
 
   fields = (_.isArray(fields)) ? fields.join():this.fields;
-
   var url = "https://api.tradeking.com/v1/market/ext/quotes.json?symbols=" + symbols;
   if(_.isString(fields)) {
     url += "&fids=" + fields;
   }
 
-  this.connection.get(url, 
-    this._credentials.access_token, 
-    this._credentials.access_secret,
-    function(error, data, response) {
-      if(error) {
-        process.nextTick(function() {
-          callback(error)
-        });
+  return new Promise(function(resolve, reject) {
+    this.__getTradeKingData__(url).then(function(data, response) {
+      try {
+        var result = JSON.parse(data).response;
+        resolve(result);
+      } catch(error) {
+        reject(error);
       }
-      else {
+    }).catch(function(error) {
+      reject(error);
+    });
+  }.bind(this));
+}
+
+TradeKing.prototype.getMarketClock = function() {
+  return new Promise(function(resolve, reject) {
+    this.__getTradeKingData__("https://api.tradeking.com/v1/market/clock.json").then(function(data, response) {
         try {
           var result = JSON.parse(data).response;
-          process.nextTick(function() {
-            callback(null, result)
-          });
+          resolve(result);
         }
         catch(error) {
-          process.nextTick(function() {
-            callback(error)
-          });
+          reject(error);
+        }
+    }).catch(function(error) {
+      reject(error);
+    });
+  }.bind(this));
+};
+
+TradeKing.prototype.__getTradeKingData__ =  function(url) {
+  return new Promise(function(resolve, reject) {
+    this.connection.get(url, 
+      this._credentials.access_token, 
+      this._credentials.access_secret,
+      function(error, data, response) {
+        if(error) {
+          reject(error);
+        }
+        else {
+          resolve(data, response);
         }
       }
-    }
-  );
-};
+    );
+  }.bind(this));
+}
 
 module.exports = TradeKing;
